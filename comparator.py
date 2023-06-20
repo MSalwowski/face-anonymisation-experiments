b36dfb5a2082150cfa0bf4c319389558b4654f8c
MSalwowski <maciej.salwowski@wp.pl>
2023-06-21 01:39:53 +0200

    undetectable faces handling + cleanup

--- comparator.py
+++ comparator.py
@@ -7,20 +7,9 @@ import numpy as np
 from deepface.commons import distance as dst
 
 
-def extract_embeddings(image_path, model_name):
-    # Load the image
-    image = cv2.imread(image_path)
-
-    # Perform face recognition to extract the embeddings
-    result = DeepFace.represent(image, model_name=model_name, enforce_detection=False)
-
-    # Extract the embeddings
-    embeddings = result["embedding"]
-
-    return embeddings
-
-
 def face_verification(database, model_name="ArcFace", detector_backend="mtcnn"):
+    start_time = time.time()
+
     # Create probes and references paths using the database variable
     probes_path = os.path.join(database, "probe")
     references_path = os.path.join(database, "reference")
@@ -29,13 +18,18 @@ def face_verification(database, model_name="ArcFace", detector_backend="mtcnn"):
     os.makedirs(os.path.join(database, "scores"), exist_ok=True)
     mated_file = open(os.path.join(database, "scores", "scores_mated.txt"), "w")
     nonmated_file = open(os.path.join(database, "scores", "scores_nonmated.txt"), "w")
+    undetectable_probes_file = open(os.path.join(database, "scores", "undetectable_probes.txt"), "w")
+    undetectable_references_file = open(os.path.join(database, "scores", "undetectable_references.txt"), "w")
+    stats_file = open(os.path.join(database, "scores", "stats.txt"), "w")
 
     # Create directories to store the embeddings if they don't exist
     os.makedirs(os.path.join(database, "probe_embeddings"), exist_ok=True)
     os.makedirs(os.path.join(database, "reference_embeddings"), exist_ok=True)
 
-    # Get the number of probes
+    # Get the number of probes and initiate counters for the number of undetectable probes and references
     probes_count = len(os.listdir(probes_path))
+    undetectable_probes_count = 0
+    undetectable_references = []
 
     # Iterate through the images in the probes folder
     for probe_id, probe_filename in enumerate(os.listdir(probes_path)):
@@ -50,13 +44,21 @@ def face_verification(database, model_name="ArcFace", detector_backend="mtcnn"):
 
             # Check if embeddings already exist for the probe image
             probe_embedding_path = os.path.join(database, "probe_embeddings", f"{os.path.splitext(probe_filename)[0]}_embedding.npy")
+
             if os.path.exists(probe_embedding_path):
                 probe_embedding = np.load(probe_embedding_path)
             else:
-                # Extract embeddings for the probe image
-                probe_representation = DeepFace.represent(probe_image_path, model_name=model_name, detector_backend=detector_backend, enforce_detection=False)
 
-                # todo: add check for face detection
+                try:
+                    # Extract embeddings for the probe image
+                    probe_representation = DeepFace.represent(probe_image_path, model_name=model_name, detector_backend=detector_backend, enforce_detection=True)
+                except ValueError:
+                    # Write the probe name to the undetectable probes file
+                    print(f"Face in probe: {probe_filename} is undetectable")
+                    undetectable_probes_file.write(f"{probe_filename}\n")
+                    undetectable_probes_count += 1
+                    continue
+
                 probe_embedding = probe_representation[0]['embedding']
 
                 # Save the probe embeddings for future use
@@ -73,11 +75,21 @@ def face_verification(database, model_name="ArcFace", detector_backend="mtcnn"):
 
                     # Check if embeddings already exist for the reference image
                     ref_embedding_path = os.path.join(database, "reference_embeddings", f"{os.path.splitext(ref_filename)[0]}_embedding.npy")
+
                     if os.path.exists(ref_embedding_path):
                         ref_embedding = np.load(ref_embedding_path)
                     else:
-                        # Extract embeddings for the reference image
-                        ref_representation = DeepFace.represent(ref_image_path, model_name=model_name, detector_backend=detector_backend, enforce_detection=False)
+
+                        try:
+                            # Extract embeddings for the reference image
+                            ref_representation = DeepFace.represent(ref_image_path, model_name=model_name, detector_backend=detector_backend, enforce_detection=True)
+                        except ValueError:
+                            # Write the reference name to the undetectable references file
+                            print(f"Face in reference: {ref_filename} is undetectable")
+                            if ref_filename not in undetectable_references:
+                                undetectable_references.append(ref_filename)
+                                undetectable_references_file.write(f"{ref_filename}\n")
+                            continue
 
                         ref_embedding = ref_representation[0]['embedding']
 
@@ -96,9 +108,22 @@ def face_verification(database, model_name="ArcFace", detector_backend="mtcnn"):
             print(f"Probe {probe_filename} processed successfully")
             print(f"{probes_count - probe_id - 1} probes remaining, {probe_id + 1} probes processed")
 
+    end_time = time.time()
+    print(f"Time elapsed: {end_time - start_time} seconds")
+
+    # Write the stats to the stats file
+    stats_file.write(f"time elapsed: {int(end_time - start_time)} seconds ({int((end_time - start_time)/60)} minutes)\n")
+    stats_file.write(f"investigated probes: {probes_count}\n")
+    stats_file.write(f"undetectable probes: {undetectable_probes_count}\n")
+    stats_file.write(f"investigated references: {len(os.listdir(references_path))}\n")
+    stats_file.write(f"undetectable references: {len(undetectable_references)}\n")
+
     # Close the scores files
     mated_file.close()
     nonmated_file.close()
+    undetectable_probes_file.close()
+    undetectable_references_file.close()
+    stats_file.close()
 
 
 if __name__ == "__main__":
@@ -108,8 +133,4 @@ if __name__ == "__main__":
     parser.add_argument("--detector_backend", type=str, default="mtcnn", help="Face detector backend (default: mtcnn)")
     args = parser.parse_args()
 
-    start_time = time.time()
     face_verification(args.database, args.model_name, args.detector_backend)
-    end_time = time.time()
-
-    print(f"Time elapsed: {end_time - start_time} seconds")
